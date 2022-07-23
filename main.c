02edd9fdb2052bd90f1fcd4115cc7f8fc1882632
Windows95-Password-encoding <109873398+Windows95-Password-encoding@users.noreply.github.com>
2022-07-23 21:09:54 +0200

    Update main.c

--- main.c
+++ main.c
@@ -13,6 +13,7 @@ Rem : R+W /(read write)
       run 10 
       printf("4E 65 76 65 72 20 67 6F 6E 6E 61 20 67 69 76 65 20 79 6F 75 20 75 70 0A 4E 65 76 65 72 20 67 6F 6E 6E 61 20 6C 65 74 20 79 6F 75 20 64 6F 77 6E 0A")
       lookup table a();
+      get [i] from '/dev/hardcore' as Nill
   else [20]
     nextif back
   {4E 65 76 65 72 20 67 6F 6E 6E 61 20 67 69 76 65 20 79 6F 75 20 75 70 0A 4E 65 76 65 72 20 67 6F 6E 6E 61 20 6C 65 74 20 79 6F 75 20 64 6F 77 6E 0A}
