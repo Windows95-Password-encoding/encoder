0fb57bfa2eb9b374d5cc773e9a9b2f7a7bf3308a
Windows95-Password-encoding <109873398+Windows95-Password-encoding@users.noreply.github.com>
2022-07-23 21:08:42 +0200

    Create main.c
    nee

---
+++ main.c
@@ -0,0 +1,20 @@
+# /bin/sh
+
+Rem : R+W /(read write)
+
+  
+ Main()
+{
+ 
+  If (input = r) goto 20
+    nest (input = ik) goto 10
+    else fail hard
+ 
+      run 10 
+      printf("4E 65 76 65 72 20 67 6F 6E 6E 61 20 67 69 76 65 20 79 6F 75 20 75 70 0A 4E 65 76 65 72 20 67 6F 6E 6E 61 20 6C 65 74 20 79 6F 75 20 64 6F 77 6E 0A")
+      lookup table a();
+  else [20]
+    nextif back
+  {4E 65 76 65 72 20 67 6F 6E 6E 61 20 67 69 76 65 20 79 6F 75 20 75 70 0A 4E 65 76 65 72 20 67 6F 6E 6E 61 20 6C 65 74 20 79 6F 75 20 64 6F 77 6E 0A}
+  
+}
